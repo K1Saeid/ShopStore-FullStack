2bb0061b071160961694fcff2aac429ab96b415f
Saeid <saeid.a.hal@gmail.com>
2025-12-03 15:34:02 +0100

    sdf

---
+++ backend/DTOs/ProductCreateDto.cs
@@ -0,0 +1,19 @@
+﻿namespace ShopStore.DTOs
+{
+    public class ProductCreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public string Gender { get; set; }
+        public string Sizes { get; set; }
+        public string Colors { get; set; }
+        public decimal Price { get; set; }
+        public decimal DiscountPrice { get; set; }
+        public bool InStock { get; set; }
+        public int ItemsLeft { get; set; }
+        public string ImageUrl { get; set; }
+        public string Slug { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
